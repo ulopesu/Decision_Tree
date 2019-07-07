186fe7d433bc14611991411321f4aa84f287fc9d
ulopesu <ufljr.1997@gmail.com>
2019-07-06 22:14:33 -0300

    FAZER DESEMPATE VALUE_STR

--- src/DecisionTree.hs
+++ src/DecisionTree.hs
@@ -5,25 +5,7 @@ import Value
 import InformationGain
 
 
- --PSEUDO-CODIGO
-{-
-arvoreDecisao (exemplos, caracteristicas, maisComum): arvore
-  se (exemplos é vazio) entao retorne maisComum;
-  senão se (todos os exemplos têm a mesma classificação) entao retorne (a classificação);
-  senão se (não há mais características) então retorne maioria(exemplos);
-  senão
-    melhor <- melhorTeste(características,exemplos);
-    árvore <- nova árvore com raiz “melhor”;
-
-    para cada valor vi de melhor faça
-      exemplosi <- exemplos onde melhor = vi;
-      subárvore <- arvoreDecisao(exemplosi, caracteristicas-{melhor}, maioria(exemplos));
-      
-      
-      adicione subárvore como um ramo à árvore com rótulo vi;
-
-  retorne arvore;
--}
+--ESTUDAR DESEMPATE PARA ValueSTR
 
 data ArvDec = Leaf String | Val Value | Root (String, [ArvDec]) deriving (Show)
 
