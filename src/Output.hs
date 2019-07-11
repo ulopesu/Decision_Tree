b4d4e2e505549e3ecdc9aab8bac8c237c4d411fc
ulopesu <ufljr.1997@gmail.com>
2019-07-11 19:28:57 -0300

    Acerta TABS e ULTIMO RABA N

--- src/Output.hs
+++ src/Output.hs
@@ -50,33 +50,42 @@ calcResultValue (ValueInt (id0, id1, decisions)) value | (length decisions) > 1 
 generateArvSTR (Leaf decision) tab = []
 generateArvSTR (Tree (nameF, idF, valuesAndTrees)) tab = generateArvStrVTS nameF valuesAndTrees tab
 
+
 generateArvStrVTS nameF [] tab = []
-generateArvStrVTS nameF (vt:vts) tab | (next /= []) && (new /= []) = saida0 | (new /= []) = saida1 | otherwise = []
+generateArvStrVTS nameF (vt:vts) tab | (next /= []) = saida0 | otherwise = new ++ tab ++ "fim-se\n"
     where new =  generateArvStrVT nameF vt tab
           next = generateArvStrVTS nameF vts (tab++"\t")
-          saida0 = new ++ tab ++ "senao\n" ++ next
-          saida1 = new ++ "fim-se\n" 
+          saida0 = new ++ tab ++ "senao\n" ++ next ++ tab ++ "fim-se\n"
 
 
-generateArvStrVT nameF (value, tree) tab = genArvStrV ++ nexTree
+generateArvStrVT nameF (value, tree) tab | (not leafTree) = genArvStrV ++ nexTree | otherwise = genArvStrV 
     where genArvStrV = generateArvStrValue value nameF tab
-          nexTree = generateArvSTR tree tab
+          nexTree = generateArvSTR tree (tab++"\t")
+          leafTree = isLeafTree tree
 
 
-generateArvStrValue (ValueStr (vName, decisions)) nameF tab | lenDe > 1 = [] | otherwise = saida
+generateArvStrValue (ValueStr (vName, decisions)) nameF tab | lenDe > 1 = saida1 | otherwise = saida0
     where lenDe = length decisions
           decision = head decisions
-          saida = tab ++ "se " ++ nameF ++ " = "++ vName ++ " entao\n" ++ tab ++"\tretorne " ++ decision ++ "\n" ++ tab ++ "fim-se\n"
+          saida0 = tab ++ "se " ++ nameF ++ " = "++ vName ++ " entao\n" ++ tab ++"\tretorne " ++ decision ++ "\n"
+          saida1 = tab ++ "se " ++ nameF ++ " = "++ vName ++ " entao\n" 
 
 
-generateArvStrValue (ValueInt (id0, id1, decisions)) nameF tab | lenDe > 1 = [] 
-                                                               | id0 == 0 = saida0
-                                                               | id0 == id1 = saida1
-                                                               | otherwise = saida2
+generateArvStrValue (ValueInt (id0, id1, decisions)) nameF tab | (lenDe > 1) && (id0 == 0) = saida0
+                                                               | (lenDe > 1) && (id0 == id1) = saida1
+                                                               | (lenDe > 1) = saida2
+                                                               | id0 == 0 = saida3
+                                                               | id0 == id1 = saida4
+                                                               | otherwise = saida5
     where lenDe = length decisions
           decision = head decisions
-          saida0 = tab ++ "se " ++ nameF ++ " <= "++ (show id1) ++ " entao\n" ++ tab ++ "\tretorne " ++ decision ++"\n" ++ tab ++ "fim-se\n"
-          saida1 = tab ++ "se " ++ nameF ++ " > "++ (show id0) ++ " entao\n" ++ tab ++ "\tretorne " ++ decision ++"\n" ++ tab ++ "fim-se\n"
-          saida2 = tab ++ "se " ++ (show id0) ++ " > " ++ nameF ++ " <= " ++ (show id1)  ++ " entao\n" ++ tab ++ "\tretorne " ++ decision ++ "\n" ++ tab ++ "fim-se\n"
+          saida0 = tab ++ "se " ++ nameF ++ " <= "++ (show id1) ++ " entao\n" 
+          saida1 = tab ++ "se " ++ nameF ++ " > "++ (show id0) ++ " entao\n"
+          saida2 = tab ++ "se " ++ (show id0) ++ " > " ++ nameF ++ " <= " ++ (show id1)  ++ " entao\n"
+          saida3 = tab ++ "se " ++ nameF ++ " <= "++ (show id1) ++ " entao\n" ++ tab ++ "\tretorne " ++ decision ++"\n"
+          saida4 = tab ++ "se " ++ nameF ++ " > "++ (show id0) ++ " entao\n" ++ tab ++ "\tretorne " ++ decision ++"\n"
+          saida5 = tab ++ "se " ++ (show id0) ++ " > " ++ nameF ++ " <= " ++ (show id1)  ++ " entao\n" ++ tab ++ "\tretorne " ++ decision ++ "\n"
+
+
 
 
